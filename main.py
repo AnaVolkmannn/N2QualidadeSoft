a31bc2bbea26abd15f8ba5be25a462b6c8a34b9a
Ana Volkmann <melodyycat@gmail.com>
2026-05-16 18:42:14 -0300

    implementação algoritmo análise de CBO

--- main.py
+++ main.py
@@ -1,28 +1,103 @@
+import os
+
 from repository import download_repo
 from scanner import find_java_files
 from analyzers.complexity import calculate_complexity
+from analyzers.coupling import calculate_cbo
+from core.project_classes import get_project_classes
 
-def main():
+def clear_terminal():
+    os.system("cls" if os.name == "nt" else "clear")
 
+def wait_next():
+    input("\nPressione ENTER para continuar...")
+    clear_terminal()
+
+def get_repository():
     repo_link = input("Insira o link do repositório: ")
-
     repo_path = download_repo(repo_link)
 
-    if repo_path is None:
-        return
+    return repo_path
 
+def load_java_files(repo_path):
     java_files = find_java_files(repo_path)
+    return java_files
 
-    print("\nANÁLISE DE COMPLEXIDADE\n")
-    print("\nArquivos Java encontrados:\n")
+
+def load_project_classes(java_files):
+    project_classes = get_project_classes(java_files)
+    return project_classes
+
+def show_complexity_analysis(java_files):
+
+    print("==============================")
+    print("ANÁLISE DE COMPLEXIDADE CICLOMÁTICA - McCabe")
+    print("==============================\n")
 
     for file in java_files:
 
         complexity = calculate_complexity(file)
 
-        print(f"{file} -> Complexidade McCabe: {complexity}")
+        print(f"{file}")
+        print(f"Complexidade McCabe: {complexity}\n")
 
-    print(f"\nTotal: {len(java_files)} arquivos")
+    print(f"Total de arquivos Java encontrados: {len(java_files)}")
+
+def show_project_classes(project_classes):
+
+    print("==============================")
+    print("CLASSES DO PROJETO")
+    print("==============================\n")
+
+    for classe in project_classes:
+        print(classe)
+
+def show_cbo_analysis(java_files, project_classes):
+
+    print("==============================")
+    print("ANÁLISE CBO")
+    print("==============================")
+
+    for file in java_files:
+
+        result = calculate_cbo(file, project_classes)
+
+        if result is None:
+            continue
+
+        print(f"\nArquivo: {result['file']}")
+        print(f"CBO: {result['cbo']}")
+
+        if result['cbo'] > 0:
+
+            print("Classes usadas:")
+
+            for classe in result['classes_used']:
+                print(f"- {classe}")
+
+
+def main():
+
+    clear_terminal()
+    repo_path = get_repository()
+
+    if repo_path is None:
+        return
+
+    java_files = load_java_files(repo_path)
+    project_classes = load_project_classes(java_files)
+
+    clear_terminal()
+    show_complexity_analysis(java_files)
+    wait_next()
+    show_project_classes(project_classes)
+    wait_next()
+    show_cbo_analysis(java_files, project_classes)
+    wait_next()
+
+    print("==============================")
+    print("FIM DA ANÁLISE")
+    print("==============================")
 
 
 if __name__ == "__main__":
